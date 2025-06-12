6f61b729ae46cd1f053f057271cf1db9ef33d60b
Mai Thanh Long <ojtpartner@sendo.vn>
2025-06-12 08:46:37 +0700

    bypass cors

--- cmd/baseCmd.go
+++ cmd/baseCmd.go
@@ -49,10 +49,10 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
+		//if c.Request.Method == "OPTIONS" {
+		//	c.AbortWithStatus(204)
+		//	return
+		//}
 		c.Next()
 	}
 }
